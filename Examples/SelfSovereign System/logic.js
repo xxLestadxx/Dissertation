b889e4538e47a5b769b191abad88ac1fcb13c03a
xxLestadxx <j.v.gospodinov@gmail.com>
2019-02-27 23:01:49 +0000

    starting to write the logic of the SS system

---
+++ Examples/SelfSovereign System/logic.js
@@ -0,0 +1,16 @@
+/**
+ * Script logic file for the Selfsovereign identity use case
+ * It has 4 participants - person, high school, university, driving school
+ * The use case will show how the person can only provide the needed data without exposing needlessly more 
+ */
+
+
+/**
+ * Diploma creation, that is the certificate from the highschool
+ * @param {org.ssidentity.CreateDiploma} createDiploma
+ * @transaction
+ */
+
+ function CreateDiploma(createDiploma){
+     var diplomaID = createDiploma.Diplo
+ }
